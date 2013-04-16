7c1a211245ee16477b63cc9c007babab63f87d37
David Gullmarsvik <david.gullmarsvik@gmail.com>
2013-04-16 13:56:38 +0200

    Basic routing

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var http = require("http");
+var url = require("url");
 
-http.createServer(function(request, response) {
-  console.log("Request recieved");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received");
 
-console.log("Server has started");
+    route(handle, pathname);
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+
+  console.log("Server has started");
+}
+
+exports.start = start;
