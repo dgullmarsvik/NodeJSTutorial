62e95dcfd31b0fd1f7abe043374de470f522c3ed
David Gullmarsvik <david.gullmarsvik@gmail.com>
2013-04-16 14:16:45 +0200

    Async request handlers

--- router.js
+++ router.js
@@ -1,9 +1,13 @@
-function route(handle, pathname) {
+function route(handle, pathname, response) {
   console.log("About to route request for " + pathname);
+  
   if (typeof handle[pathname] === 'function') {
-    handle[pathname]();
+    handle[pathname](response);
   } else {
     console.log("No request handler found for " + pathname);
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(content);
+    response.end();
   }
 }
 
